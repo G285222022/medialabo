eeab3dbcfa6feed46d669f2f1dc5417c45d6f012
G285222022 <228522@st.takushoku-u.ac.jp>
2023-07-26 21:21:20 +0900

    a

--- weather.js
+++ weather.js
@@ -56,3 +56,60 @@ console.log(data.main.humidity);
 console.log(data.wind.speed);
 console.log(data.wind.deg);
 console.log(data.name);
+
+
+
+let a = document.querySelector('#btn');
+  a.addEventListener('click',sendRequest);
+  
+  function sendRequest(){
+    let s = document.querySelector('select#kunimei');
+    let idx = s.selectedIndex;  
+  
+    let os = s.querySelectorAll('option');  
+    let o = os.item(idx);       
+    let id = o.getAttribute('id');
+    let url='https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
+  
+    axios.get(url)
+          .then(showResult)   
+          .catch(showError)   
+          .then(finish);      
+  }
+  
+  function showResult(resp) {
+    let data = resp.data;
+  
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+    console.log(data);
+    console.log(data.x);
+  
+    let toshi =document.querySelector('caption.toshi');
+    toshi.textContent = data.name;
+    let lat =document.querySelector('td.lat');
+    lat.textContent = data.coord.lat;
+    let lon = document.querySelector('td.lon');
+    lon.textContent = data.coord.lon;
+    let weather = document.querySelector('td.weather');
+    weather.textContent = data.weather[0].description;
+    let temp_min = document.querySelector('td.temp_min');
+    temp_min.textContent = data.main.temp_min;
+    let temp_max = document.querySelector('td.temp_max');
+    temp_max.textContent = data.main.temp_max;
+    let humidity = document.querySelector('td.humidity');
+    humidity.textContent = data.main.humidity;
+    let speed = document.querySelector('td.speed');
+    speed.textContent = data.wind.speed;
+    let deg = document.querySelector('td.deg');
+    deg.textContent = data.wind.deg;
+  }
+  
+  function showError(err) {
+    console.log(err);
+  }
+  
+  function finish() {
+    console.log('Ajax 通信が終わりました');
+  }
